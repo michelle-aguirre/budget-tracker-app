a8262ad4932516979c54384b01d7373bee8b5054
michelle-aguirre <94195510+michelle-aguirre@users.noreply.github.com>
2022-06-01 11:40:39 -0400

    transaction, manifest, and service worker

---
+++ Main/service-worker.js
@@ -0,0 +1,32 @@
+const APP_PREFIX = 'my-site-cache-';
+const VERSION = 'v1';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
+
+const FILES_TO_CACHE = [
+    "/",
+    "./index.html",
+    "./css/styles.css",
+    "./js/idb.js",
+    "./js/index.js",
+    "./manifest.json",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
+];
+
+// Cache resources
+self.addEventListener("install", function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
