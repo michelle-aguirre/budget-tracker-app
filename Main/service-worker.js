69cb4287bc536ceee5b477d2cc219bb72326ab45
michelle-aguirre <94195510+michelle-aguirre@users.noreply.github.com>
2022-06-01 11:55:02 -0400

    continued coding service-worker.js

--- Main/service-worker.js
+++ Main/service-worker.js
@@ -30,3 +30,64 @@ self.addEventListener("install", function(event) {
         })
     );
 });
+
+// Respond with caches resources
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    // If the response was good, clone it and store it in the cache.
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch(err => {
+                    // Network request failed, try to get it from the cache.
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match("/");
+                }
+            });
+        })
+    );
+});
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // `keylist` contains all cache names under your username.github.io
+            //filter out oness that has this app prefix to create white list
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            // add current cache name to white list
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keylist.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
+});
